532acb781ec5e857f55948f0e36810c9ac870262
Gerard Ballestero Vidal <6224745@cegepmontpetit.ca>
2025-11-05 11:14:36 -0500

    FCT Création model/contoller Score et basse de données

---
+++ flappyBirb_serveur/Models/Score.cs
@@ -0,0 +1,12 @@
+﻿namespace flappyBirb_serveur.Models
+{
+    public class Score
+    {
+        public int Id { get; set; }
+        public string? Pseudo { get; set; } = null;
+        public string? Date { get; set; } = null;
+        public int TimeInSeconds { get; set; }
+        public int ScoreValue { get; set; }
+        public bool IsPublic { get; set; }
+    }
+}
